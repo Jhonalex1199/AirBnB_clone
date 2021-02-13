eb998388c9c98281fe22ef82892048ff5244d2f0
jhonalex1199 <2050@holbertonschool.com>
2021-02-12 20:08:03 -0500

    args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -5,35 +5,38 @@ import datetime
 
 
 class BaseModel:
-    """a documentation of my class:
+    """a documentation of my class:\
     defines all common attributes/methods\
     for other classes:"""
 
-    def __init__(self, id="", *args, **kwargs)):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.update_at = created_at
-
+    def __init__(self, *args, **kwargs)):
         if kwargs:
-            
+            for key, val in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "update_at":
+                        val=datetime.datetime.strptime(val, "\
+                        %Y-%m-%dT%H:%M:%S.%f")
+                        setattr(self, key, val)
+
+        self.id=str(uuid.uuid4())
+        self.created_at=datetime.datetime.now()
+        self.update_at=created_at
 
     def __str__(self):
         """string representation"""
+        return '[{}]'.format(self.__class__.__name__) + '\
+        ({})'.format(self.id) + '{}'.format(self.__dict__)
 
-        return '[{}]'.format(self.__class__.__name__) + '({})'.format(self.id) + '{}'.format(self.__dict__)
-        
     def save(self):
-        """ updates the public instance attribute updated_at"""
-
-        self.update_at = datetime.datetime.now()
+        """updates the public instance attribute updated_at"""
+        self.update_at=datetime.datetime.now()
         return self.update_at
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
         of __dict__ of the instance"""
-
-        dict_a = self.__dict__
-        dict_a["created_at"] = self.created_at.isoformat()
-        dict_a["update_at"] = self.update_at.isoformat()
-        dict_a["__class__"] = self.__class__.__name__
+        dict_a=self.__dict__
+        dict_a["created_at"]=self.created_at.isoformat()
+        dict_a["update_at"]=self.update_at.isoformat()
+        dict_a["__class__"]=self.__class__.__name__
         return dict_a
