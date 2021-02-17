81c1bb9fa234b3228e09827ce04a8b82d811c2c8
jhonalex1199 <2050@holbertonschool.com>
2021-02-17 15:29:57 -0500

    first console

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""contains the entry point of the command interpreter"""
+import cmd
+import json
+from models.base_model import BaseModel
+from models import storage
+
+classes = {"BaseModel"}
+
+
+class HBNBCommand(cmd.Cmd):
+    """class definition"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """Exit the program"""
+        print("")
+        return True
+
+    def emptyline(self):
+        """shouldn’t execute anything
+        """
+        pass
+
