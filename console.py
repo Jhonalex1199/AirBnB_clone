151b2f86deb47c19d27e7f2224170013e62fa29a
tao08 <1823@holbertonschool.com>
2021-02-18 03:09:03 +0000

    upclass

--- console.py
+++ console.py
@@ -4,13 +4,22 @@ import cmd
 import json
 from models.base_model import BaseModel
 from models import storage
-
-classes = {"BaseModel"}
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+import aux_functions
+import re
 
 
 class HBNBCommand(cmd.Cmd):
-    """class definition"""
-    prompt = "(hbnb) "
+    """ Command line main module """
+    classes_list = ['BaseModel', 'User', 'City',
+                    'State', 'Place', 'Amenity', 'Review']
+    prompt = '(hbnb) '
+
 
     def do_quit(self, line):
         """Quit command to exit the program"""
