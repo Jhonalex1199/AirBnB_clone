b06bb3629a6855f22958b4d235d024df7728f0b1
tao08 <1823@holbertonschool.com>
2021-02-18 03:17:15 +0000

    upbase

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,61 @@
-#!/usr/bin/python3
-"""Basemodel of AirBnB"""
+#!/usr/bin/pyhton3
+"""Let's import the modules"""
 import uuid
 from datetime import datetime
-import json
+import models
+format = '%Y-%m-%dT%H:%M:%S.%f'
 
 
-class BaseModel:
-    """defines all common attributes/methods for other classes:"""
+class BaseModel():
+    """class defines all common attributes/methods and the
+    assign various atributes principal
+    that should tener each object
+    """
+
     def __init__(self, *args, **kwargs):
+        '''inicialization Base
+        *args won’t be used
+        if kwargs is not empty:
+        each key of this dictionary is an attribute name
+        each value of this dictionary is the value of this attribute name
+        '''
         if kwargs:
-            for key, val in kwargs.items():
-                if key != "__class__":
-                    if key == "created_at" or key == "update_at":
-                        val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, val)
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    setattr(self, k, v)
+                if k == "created_at" or k == "updated_at":
+                    form = '%Y-%m-%dT%H:%M:%S.%f'
+                    setattr(self, k, datetime.datetime.strptime(v, form))
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
-        from models.__init__ import storage
-        storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        """string representation"""
-        return '[{}]'.format(self.__class__.__name__) + '\
-                ({})'.format(self.id) + '{}'.format(self.__dict__)
+        '''human readable'''
+        return "[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at"""
-        self.update_at = datetime.now()
-        from models.__init__ import storage
-        storage.new(self)
+        '''updates the public instance attribute
+        updated_at with the current datetime
+        '''
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values
-        of __dict__ of the instance"""
-        dict_a = self.__dict__
-        dict_a["created_at"] = self.created_at.isoformat()
-        dict_a["update_at"] = self.update_at.isoformat()
-        dict_a["__class__"] = self.__class__.__name__
-        return dict_a
+        '''
+        creates dictionary of the class  and returns
+        a dictionary of all the key values in __dict__
+        '''
+        dic2 = self.__dict__.copy()
+        dic2["created_at"] = dic2["created_at"].isoformat()
+        dic2["updated_at"] = dic2["updated_at"].isoformat()
+        dic2["__class__"] = self.__class__.__name__
+
+        return dic2
 
     def reload(self):
         """ deserialize the file json
