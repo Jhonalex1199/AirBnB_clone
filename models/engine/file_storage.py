02fb76dd4e991b61ef70fdc848c7f27c3e8f2ec4
tao08 <1823@holbertonschool.com>
2021-02-18 03:23:20 +0000

    upimport

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,38 +1,50 @@
 #!/usr/bin/python3
-"""Storage"""
+""" Import the modules need it for this storage """
 import json
-from models.user import User
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+"""Future models to import in the project"""
 
 
 class FileStorage:
-    """serializes instances to a JSON file and\
-    deserializes JSON file to instances"""
+    """ File Storage Class"""
+    """FileStorage that serializes instances to a JSON file"""
+    """and deserializes JSON file to instances"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """returns the dictionary __objects"""
+        """ public instance return dictionary """
         return FileStorage.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key obj.id"""
-        FileStorage.__objects[type(obj).__name__ + "." + obj.id] = obj
+        """ public instance that sets in __objects """
+        key = type(obj).__name__ + "." + str(obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file"""
+        """ Public instance that serializes __objects """
         new_dict = {}
-        for key in FileStorage.__objects:
-            new_dict[key] = FileStorage.__objects[key].to_dict()
-        with open(FileStorage.__file_path, mode="w") as f:
-            json.dump(new_dict, f)
+        for key, value in FileStorage.__objects.items():
+            new_dict[key] = value.to_dict()
+
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as file:
+            data = json.dumps(new_dict)
+            file.write(data)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """ Public instance that deserializes JSON to object """
         try:
-            with open(FileStorage.__file_path, mode="r") as f:
-                for key, value in (json.load(f)).items():
-                    value = eval(value["__class__"])(**value)
-                    FileStorage.__objects[key] = value
+            with open(FileStorage.__file_path, 'r', encoding="UTF-8") as file:
+                list_data = json.load(file)
+                for key, value in list_data.items():
+                    FileStorage.__objects[key] = eval(
+                        value['__class__'])(**value)
+                    # State(**vale)
         except Exception:
             pass
